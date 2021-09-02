08e0ac1ae1b0801051d63d152c2f5076f2ef584c
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-09-02 16:02:12 +0530

    Add project files.

---
+++ ConsoleAppFactorialRecursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialRecursion
+{
+    class Fact
+    {
+        internal int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of given number is: " + new Fact().Factorial(n));
+            Console.ReadKey();
+        }
+    }
+}
